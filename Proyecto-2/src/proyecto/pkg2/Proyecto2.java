3ae59e030f895fa5fe449206b2288564c1b5e1e8
Pecathf <luis.arcia@correo.unimet.edu.ve>
2025-06-30 20:03:54 -0400

    Avance 30/6
    Avance del proyecto del dia 30/6 agregando 3 interfaces, 1 funcional

---
+++ Proyecto-2/src/proyecto/pkg2/Proyecto2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.pkg2;
+import Interfaces.Bienvenida;
+/**
+ *
+ * @author Angel
+ */
+public class Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bienvenida P = new Bienvenida();
+        P.setVisible(true);
+    }
+    
+}
